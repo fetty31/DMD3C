2ab63d8afbe88c717d8bc74e0bc294c0b90acf21
Sharpiless <50802787+Sharpiless@users.noreply.github.com>
2025-04-11 11:20:39 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 xvfb-run -s "-screen 0 1400x900x24" \
-    python infer2.py gpus=[2] name=BP_KITTI ++chpt=DMD3C \
+    python demo.py gpus=[2] name=BP_KITTI ++chpt=DMD3C \
     net=PMP num_workers=1 \
     data=KITTI data.testset.mode=test data.testset.height=352 \
     test_batch_size=1 metric=RMSE ++save=true
