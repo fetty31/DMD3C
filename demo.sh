6851ae295714a69cceabe5b4e4ca46007c8b81d6
Sharpiless <50802787+Sharpiless@users.noreply.github.com>
2025-05-02 15:05:51 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
-python demo.py gpus=[2] name=BP_KITTI ++chpt=ours \
+python demo.py gpus=[2] name=BP_KITTI ++chpt=checkpoints/dmd3c_kitti.pth \
     net=PMP num_workers=1 \
     data=KITTI data.testset.mode=test data.testset.height=352 \
     test_batch_size=1 metric=RMSE ++save=true
